06be40a163d33aab871d6c76acf6464407a24d49
David Stubbersfield <david.stubbersfield@uqconnect.edu.au>
2016-04-28 21:30:31 +1000

    Updated graph structure and cleaned up code.

--- src/graphs/AccelerationGraph.java
+++ src/graphs/AccelerationGraph.java
@@ -6,73 +6,45 @@ import java.util.Observer;
 
 import org.jfree.data.time.*;
 import org.jfree.ui.ApplicationFrame;
-import java.awt.Color;
-import java.awt.BasicStroke;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYSeriesCollection;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.data.time.Second;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Random;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JPanel;
-import javax.swing.Timer;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.time.DynamicTimeSeriesCollection;
-import org.jfree.data.time.Second;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
-import java.sql.Date;
 
+import javax.swing.JPanel;
 
 /**
  * Created by david on 24/04/16.
  */
 public class AccelerationGraph extends ApplicationFrame implements Observer  {
 
-    TimeSeriesCollection dataset;
     TimeSeries series;
 
     public AccelerationGraph() {
 
         super("Acceleration over time");
         this.series = new TimeSeries("Acceleration over time");
-        this.dataset = new TimeSeriesCollection(this.series);
+        TimeSeriesCollection dataset = new TimeSeriesCollection(this.series);
 
         final JFreeChart chart = ChartFactory.createTimeSeriesChart(
                 "Acceleration over time",
                 "Time(s)",
                 "Acceleration(m/s^2)",
-                dataset,
-                true,
-                true,
-                false
+                dataset
         );
 
         final XYPlot plot = chart.getXYPlot();
         ValueAxis axis = plot.getDomainAxis();
         axis.setAutoRange(true);
-        axis.setFixedAutoRange(30000.0); // 30 seconds
+        axis.setFixedAutoRange(10000.0); // 10 seconds
+
         axis = plot.getRangeAxis();
-        axis.setRange(0.0, 200.0);
+        axis.setRange(0.0, 200.0); // Acceleration
 
         final ChartPanel chartPanel = new ChartPanel(chart);
         final JPanel content = new JPanel(new BorderLayout());
@@ -85,31 +57,6 @@ public class AccelerationGraph extends ApplicationFrame implements Observer  {
         RefineryUtilities.centerFrameOnScreen(this);
         this.setVisible(true);
 
-        //dataset.setTimeBase(new Second(0, 0, 0, 1, 1, 2011));
-        //dataset.addSeries(gaussianData(), 0, "Gaussian data");
-
-        /*this.xylineChart = ChartFactory.createXYLineChart(
-                "Acceleration over time",
-                "Time(s)",
-                "Acceleration(m/s^2)",
-                //new XYSeriesCollection(),
-                dataset,
-                PlotOrientation.VERTICAL,
-                true, true, false);
-
-        ChartPanel chartPanel = new ChartPanel( xylineChart );
-        chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
-        final XYPlot plot = xylineChart.getXYPlot( );
-        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer( );
-        renderer.setSeriesPaint( 0 , Color.RED );
-        renderer.setSeriesStroke( 0 , new BasicStroke( 4.0f ) );
-        plot.setRenderer( renderer );
-        setContentPane( chartPanel );
-
-        this.pack( );
-        RefineryUtilities.centerFrameOnScreen( this );
-        this.setVisible( true );*/
-
     }
 
     @Override
@@ -117,19 +64,8 @@ public class AccelerationGraph extends ApplicationFrame implements Observer  {
 
         HashMap<String, Float> currentSimParams = (HashMap<String, Float>) arg;
         float currentAcceleration = currentSimParams.get("acceleration");
-        float currentTime = currentSimParams.get("time");
-        //long currentTimeLong = (long)currentTime;
 
         this.series.add(new Second(), currentAcceleration);
-
-
-        //this.series.add(new Millisecond(new Date(currentTimeLong)), currentAcceleration);
-
-       /* this.dataset.advanceTime();
-        float[] newData = new float[1];
-        newData[0] = 10f;
-        dataset.appendData(newData);*/
-
 
     }
 
--- src/graphs/VelocityGraph.java
+++ src/graphs/VelocityGraph.java
@@ -1,53 +1,50 @@
 package graphs;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.time.Second;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
-
-import javax.swing.*;
-import java.awt.*;
 import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
 
+import org.jfree.data.time.*;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.ui.RefineryUtilities;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.data.time.Second;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
 
 /**
  * Created by david on 24/04/16.
  */
 public class VelocityGraph extends ApplicationFrame implements Observer  {
 
-    TimeSeriesCollection dataset;
     TimeSeries series;
 
     public VelocityGraph() {
 
         super("Velocity over time");
         this.series = new TimeSeries("Velocity over time");
-        this.dataset = new TimeSeriesCollection(this.series);
+        TimeSeriesCollection dataset = new TimeSeriesCollection(this.series);
 
         final JFreeChart chart = ChartFactory.createTimeSeriesChart(
                 "Velocity over time",
                 "Time(s)",
                 "Velocity(m/s)",
-                dataset,
-                true,
-                true,
-                false
+                dataset
         );
 
         final XYPlot plot = chart.getXYPlot();
         ValueAxis axis = plot.getDomainAxis();
         axis.setAutoRange(true);
-        axis.setFixedAutoRange(30000.0); // 30 seconds
+        axis.setFixedAutoRange(10000.0); // 10 seconds
+
         axis = plot.getRangeAxis();
-        axis.setRange(0.0, 200.0);
+        axis.setRange(0.0, 200.0); // Velocity
 
         final ChartPanel chartPanel = new ChartPanel(chart);
         final JPanel content = new JPanel(new BorderLayout());
@@ -60,31 +57,6 @@ public class VelocityGraph extends ApplicationFrame implements Observer  {
         RefineryUtilities.centerFrameOnScreen(this);
         this.setVisible(true);
 
-        //dataset.setTimeBase(new Second(0, 0, 0, 1, 1, 2011));
-        //dataset.addSeries(gaussianData(), 0, "Gaussian data");
-
-        /*this.xylineChart = ChartFactory.createXYLineChart(
-                "Acceleration over time",
-                "Time(s)",
-                "Acceleration(m/s^2)",
-                //new XYSeriesCollection(),
-                dataset,
-                PlotOrientation.VERTICAL,
-                true, true, false);
-
-        ChartPanel chartPanel = new ChartPanel( xylineChart );
-        chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
-        final XYPlot plot = xylineChart.getXYPlot( );
-        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer( );
-        renderer.setSeriesPaint( 0 , Color.RED );
-        renderer.setSeriesStroke( 0 , new BasicStroke( 4.0f ) );
-        plot.setRenderer( renderer );
-        setContentPane( chartPanel );
-
-        this.pack( );
-        RefineryUtilities.centerFrameOnScreen( this );
-        this.setVisible( true );*/
-
     }
 
     @Override
@@ -92,19 +64,8 @@ public class VelocityGraph extends ApplicationFrame implements Observer  {
 
         HashMap<String, Float> currentSimParams = (HashMap<String, Float>) arg;
         float currentVelocity = currentSimParams.get("velocity");
-        float currentTime = currentSimParams.get("time");
-        //long currentTimeLong = (long)currentTime;
 
         this.series.add(new Second(), currentVelocity);
-
-
-        //this.series.add(new Millisecond(new Date(currentTimeLong)), currentAcceleration);
-
-       /* this.dataset.advanceTime();
-        float[] newData = new float[1];
-        newData[0] = 10f;
-        dataset.appendData(newData);*/
-
 
     }
 
